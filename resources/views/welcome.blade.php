4bbe43df5a44ee8358691106132293a471db8526
frank <ndungufrank01@gmail.com>
2024-07-23 14:34:44 +0300

    layout, controllers

--- resources/views/welcome.blade.php
+++
@@ -1,1 +0,0 @@
-<h1 class="bg-red-700">Hello World</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/feed', function () {
-    return view('feed');
-});
-
-Route::get('/profile', function () {
-    return view('users.profile');
-});
+Route::get("/", [DashboardController::class,"index"]);
+Route::get("/profile", [ProfileController::class,"update"]);
